51571643a7867c88bea3ebcd49a9a603f0b34c30
Abdulazez Ho <abdhomsi55@gmail.com>
2020-03-22 18:21:38 +0200

    zoom edit

--- app/src/main/java/com/journaldev/fmfi/ScannedBarcodeActivity.java
+++ app/src/main/java/com/journaldev/fmfi/ScannedBarcodeActivity.java
@@ -3,6 +3,7 @@ package com.journaldev.fmfi;
 import android.Manifest;
 import android.app.ProgressDialog;
 import android.content.pm.PackageManager;
+import android.graphics.PointF;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.app.ActivityCompat;
@@ -34,6 +35,19 @@ import android.view.ScaleGestureDetector;
 
 public class ScannedBarcodeActivity extends AppCompatActivity {
 
+    float[] lastEvent = null;
+    float d = 0f;
+    float newRot = 0f;
+    private boolean isZoomAndRotate;
+    private boolean isOutSide;
+    private static final int NONE = 0;
+    private static final int DRAG = 1;
+    private static final int ZOOM = 2;
+    private int mode = NONE;
+    private PointF start = new PointF();
+    private PointF mid = new PointF();
+    float oldDist = 1f;
+    private float xCoOrdinate, yCoOrdinate;
 
     SurfaceView surfaceView;
     TextView txtBarcodeValue;
@@ -59,10 +73,20 @@ public class ScannedBarcodeActivity extends AppCompatActivity {
         initViews();
         mProgressDialog = new ProgressDialog(this,R.style.AppCompatAlertDialogStyle);
         mProgressDialog.setCancelable(false);
-        mScaleGestureDetector = new ScaleGestureDetector(this, new ScaleListener());
-
+        //abd
+//        mScaleGestureDetector = new ScaleGestureDetector(this, new ScaleListener());
+        myimage.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                ImageView view = (ImageView) v;
+                view.bringToFront();
+                viewTransformation(view, event);
+                return true;
+            }
+        });
     }
-    @Override
+    //abd
+   /* @Override
     public boolean onTouchEvent(MotionEvent event) {
         return mScaleGestureDetector.onTouchEvent(event);
     }
@@ -77,7 +101,89 @@ public class ScannedBarcodeActivity extends AppCompatActivity {
             myimage.setScaleY(mScaleFactor);
             return true;
         }
+    }*/
+   //abd
+    private void viewTransformation(View view, MotionEvent event) {
+        switch (event.getAction() & MotionEvent.ACTION_MASK) {
+            case MotionEvent.ACTION_DOWN:
+                xCoOrdinate = view.getX() - event.getRawX();
+                yCoOrdinate = view.getY() - event.getRawY();
+
+                start.set(event.getX(), event.getY());
+                isOutSide = false;
+                mode = DRAG;
+                lastEvent = null;
+                break;
+            case MotionEvent.ACTION_POINTER_DOWN:
+                oldDist = spacing(event);
+                if (oldDist > 10f) {
+                    midPoint(mid, event);
+                    mode = ZOOM;
+                }
+
+                lastEvent = new float[4];
+                lastEvent[0] = event.getX(0);
+                lastEvent[1] = event.getX(1);
+                lastEvent[2] = event.getY(0);
+                lastEvent[3] = event.getY(1);
+                d = rotation(event);
+                break;
+            case MotionEvent.ACTION_UP:
+                isZoomAndRotate = false;
+                if (mode == DRAG) {
+                    float x = event.getX();
+                    float y = event.getY();
+                }
+            case MotionEvent.ACTION_OUTSIDE:
+                isOutSide = true;
+                mode = NONE;
+                lastEvent = null;
+            case MotionEvent.ACTION_POINTER_UP:
+                mode = NONE;
+                lastEvent = null;
+                break;
+            case MotionEvent.ACTION_MOVE:
+                if (!isOutSide) {
+                    if (mode == DRAG) {
+                        isZoomAndRotate = false;
+                        view.animate().x(event.getRawX() + xCoOrdinate).y(event.getRawY() + yCoOrdinate).setDuration(0).start();
+                    }
+                    if (mode == ZOOM && event.getPointerCount() == 2) {
+                        float newDist1 = spacing(event);
+                        if (newDist1 > 10f) {
+                            float scale = newDist1 / oldDist * view.getScaleX();
+                            view.setScaleX(scale);
+                            view.setScaleY(scale);
+                        }
+                        if (lastEvent != null) {
+                            newRot = rotation(event);
+                            view.setRotation((float) (view.getRotation() + (newRot - d)));
+                        }
+                    }
+                }
+                break;
+        }
     }
+
+    private float rotation(MotionEvent event) {
+        double delta_x = (event.getX(0) - event.getX(1));
+        double delta_y = (event.getY(0) - event.getY(1));
+        double radians = Math.atan2(delta_y, delta_x);
+        return (float) Math.toDegrees(radians);
+    }
+
+    private float spacing(MotionEvent event) {
+        float x = event.getX(0) - event.getX(1);
+        float y = event.getY(0) - event.getY(1);
+        return (int) Math.sqrt(x * x + y * y);
+    }
+
+    private void midPoint(PointF point, MotionEvent event) {
+        float x = event.getX(0) + event.getX(1);
+        float y = event.getY(0) + event.getY(1);
+        point.set(x / 2, y / 2);
+    }
+
     private void initViews() {
         txtBarcodeValue = findViewById(R.id.txtBarcodeValue);
         surfaceView = findViewById(R.id.surfaceView);
